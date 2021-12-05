98d48d799ef116e6b2d1548f65af3a1632d1e82b
pschuetz <paul.schuetz@inovex.de>
2021-12-05 11:13:57 +0100

    day05 first star

---
+++ src/main/kotlin/Day05.kt
@@ -0,0 +1,42 @@
+data class Coordinate(
+    val x: Int,
+    val y: Int
+)
+
+data class HydrothermalVentureLine(
+    val start: Coordinate,
+    val end: Coordinate
+)
+
+/**
+ * Calculates the number of coordinates where at least two hydrothermal ventures 🌪 overlap.
+ */
+fun calculateHydrothermalVentureThreat(lines: List<HydrothermalVentureLine>): Int {
+    // first level corresponds to y coordinate, second level is x coordinate
+    val area = mutableMapOf<Int, MutableMap<Int, Int>>()
+
+    for (line in lines) {
+        // x coordinates are equal so we have a vertical line
+        if (line.start.x == line.end.x) {
+            for (y in line.start.y toward line.end.y) {
+                area.computeIfAbsent(y) { mutableMapOf() }
+                    .compute(line.start.x) { _, v -> if (v == null) 1 else v + 1 }
+            }
+        }
+        // y coordinates are equal, and we have a horizontal line
+        if (line.start.y == line.end.y) {
+            for (x in line.start.x toward line.end.x) {
+                area.computeIfAbsent(line.start.y) { mutableMapOf() }
+                    .compute(x) { _, v -> if (v == null) 1 else v + 1 }
+            }
+        }
+    }
+
+    // return number of coordinates where there was more than one hydrothermal venture line.
+    return area.values.sumOf { it.values.count { coordinate -> coordinate > 1 } }
+}
+
+private infix fun Int.toward(to: Int): IntProgression {
+    val step = if (this > to) -1 else 1
+    return IntProgression.fromClosedRange(this, to, step)
+}
---
+++ src/test/kotlin/Day05Tests.kt
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day05Tests {
+    @Test
+    fun calculateHydrothermalVentureThreatTest() {
+        val overlaps = calculateHydrothermalVentureThreat(
+            lines = listOf(
+                HydrothermalVentureLine(start = Coordinate(0, 9), end = Coordinate(5, 9)),
+                HydrothermalVentureLine(start = Coordinate(8, 0), end = Coordinate(0, 8)),
+                HydrothermalVentureLine(start = Coordinate(9, 4), end = Coordinate(3, 4)),
+                HydrothermalVentureLine(start = Coordinate(2, 2), end = Coordinate(2, 1)),
+                HydrothermalVentureLine(start = Coordinate(7, 0), end = Coordinate(7, 4)),
+                HydrothermalVentureLine(start = Coordinate(6, 4), end = Coordinate(2, 0)),
+                HydrothermalVentureLine(start = Coordinate(0, 9), end = Coordinate(2, 9)),
+                HydrothermalVentureLine(start = Coordinate(3, 4), end = Coordinate(1, 4)),
+                HydrothermalVentureLine(start = Coordinate(0, 0), end = Coordinate(8, 8)),
+                HydrothermalVentureLine(start = Coordinate(5, 5), end = Coordinate(8, 2)),
+            )
+        )
+
+        Assertions.assertEquals(5, overlaps)
+    }
+
+    @Test
+    fun calculateHydrothermalVentureThreatSolution() {
+        val hydrothermalVentureLines = File("src/test/resources/input-05.txt").readLines()
+            .map { rawToHydrothermalVentureLine(it) }
+
+        val overlaps = calculateHydrothermalVentureThreat(hydrothermalVentureLines)
+        println("We found $overlaps overlaps of hydrothermal ventures 🌪 You better watch out.")
+    }
+
+    private fun rawToHydrothermalVentureLine(rawLine: String): HydrothermalVentureLine {
+        val (start, end) = rawLine.split("->").map { it.trim() }
+        val (startX, startY) = start.split(",").map { it.trim().toInt() }
+        val (endX, endY) = end.split(",").map { it.trim().toInt() }
+        return HydrothermalVentureLine(
+            start = Coordinate(x = startX, y = startY),
+            end = Coordinate(x = endX, y = endY)
+        )
+    }
+}
